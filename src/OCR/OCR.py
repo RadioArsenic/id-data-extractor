96c7c4a5f55d92122049a8e918eaae9f07322b8e
Amy Neve <ladyames20203@gmail.com>
2023-10-13 00:01:11 +0800

    todo list and copied over date and address parsing

--- src/OCR/OCR.py
+++ src/OCR/OCR.py
@@ -3,6 +3,26 @@ import numpy as np
 import pytesseract
 import json
 
+# TODO
+"""
+!Account for skewing
+
+adding roi coords:
+    ACT name roi (last is before first)
+    TAS roi 
+
+fixing roi:
+    VIC, NT, SA all have errors
+    account for long and short names
+
+
+Cleaning data
+    parse dates into same format (' ', '-', '/', '.')
+            QLD does diff style to rest again
+    address same format (remove commas)
+    name upper vs lower case
+"""
+
 
 class ImageConstantROI:
     class CCCD(object):
@@ -163,6 +183,7 @@ def extract_information(image_path, location):
 # SA: "1 FIRST ST ADELAIDE 5000" vs "1 FIRST S” ADELAIDE 50C0", "13/09/2014" vs "43709/2014", "14/09/1995" vs "14:99;1995"
 # print(extract_information("./test_images/SA-driver-license.png", "AUSTRALIA_SA"))
 
+# clean output: text = re.sub("[^ \na-zA-Z\d'\/-]*", "", text)
 
 # * INFO WANTED:
 # name
@@ -174,3 +195,105 @@ def extract_information(image_path, location):
 # convert string to python dictionary then to json
 def parsetoJSON(text):
     pass
+
+
+def address_detection(text):
+    regexp = r"\d{1,4}(?:\s+[A-Za-z]+){3,}\s+\d{4,5}"
+    address = re.findall(regexp, text)
+    print(address)
+    return address
+
+
+def date_builder(day, month, year):
+    """formats the date"""
+    return f"{day}-{month}-{year}"
+
+
+def month_conversion(month):
+    """converts the month from letter to number format"""
+    r_month = ""
+    month = month.upper()
+    month_dict = {
+        "JAN": "01",
+        "FEB": "02",
+        "MAR": "03",
+        "APR": "04",
+        "MAY": "05",
+        "JUN": "06",
+        "JUL": "07",
+        "AUG": "08",
+        "SEP": "09",
+        "OCT": "10",
+        "NOV": "11",
+        "DEC": "12",
+    }
+    return month_dict.get(month, "00")
+
+
+def date_detection(text):
+    """a function to detect dates in a variety of forms and transform
+    them into the typical dd-mm-yyyy format"""
+    # an array to hold all discovered dates
+    dates = []
+
+    # regex patterns
+    pattern = r"\b\d{16}\b"  # 16 numbers
+    pattern2 = r"\d{2}[A-Za-z]{3}\d{4}"  # 10JUN1990
+    pattern3 = r"[0-9]{2}-[A-Z]{3}-[0-9]{4}"  # 10-JUN-1990
+    pattern4 = r"[0-9]{2}-[0-9]{2}-[0-9]{4}"  # 10-06-1990
+    pattern5 = r"[0-9]{2}/[A-Z]{3}/[0-9]{4}"  # 10/JUN/1990
+    pattern6 = r"[0-9]{2}/[0-9]{2}/[0-9]{4}"  # 10/09/1990
+    pattern8 = r"\b\d{8}\b"  # 8 numbers
+
+    # 16 numbers
+    matches_16 = re.findall(pattern, text)
+    for i in matches_16:
+        j = i[8:]
+        dates.append(date_builder(i[:2], i[2:4], i[4:8]))
+        dates.append(date_builder(j[:2], j[2:4], j[4:8]))
+
+    # 8 numbers
+    matches_8 = re.findall(pattern8, text)
+    for i in matches_8:
+        dates.append(date_builder(i[:2], i[2:4], i[4:]))
+
+    # ddMMMyyyy
+    matches_3m = re.findall(pattern2, text)
+    for i in matches_3m:
+        month = month_conversion(i[2:5])
+        dates.append(date_builder(i[:2], month, i[5:]))
+
+    # dd-MMM-yyyy dd/MMM/yyyy
+    matches_3dash = re.findall(pattern3, text)
+    matches_3dash.extend(re.findall(pattern5, text))
+    for i in matches_3dash:
+        month = month_conversion(i[3:6])
+        dates.append(date_builder(i[:2], month, i[7:]))
+
+    # dd-mm-yyyy dd/mm/yyyy
+    matches_2dash = re.findall(pattern4, text)
+    matches_2dash.extend(re.findall(pattern6, text))
+    for i in matches_2dash:
+        dates.append(date_builder(i[:2], i[3:5], i[6:]))
+
+    return dates
+
+
+def validate_dates(dates):
+    """ensures that all dates are valid. returns 0 if
+    invalid date is present. if all dates are valid, returns
+    the oldest date (date of birth)"""
+    birthdate = ""
+    year_check = 2101
+    for i in dates:
+        day = int(i[:2])
+        month = int(i[3:5])
+        year = int(i[6:])
+        # checking date is valid
+        if 32 < day < 0 or 13 < month < 0 or 2100 < year < 1900:
+            return 0
+        else:
+            # if the current date is older than the stored date
+            if year < year_check:
+                birthdate = i
+    return birthdate
